83519d4c5a5e78cf01d7301a55ab392eac13aeda
Gcarreiro <gcd1998@hotmail.com>
2024-10-21 11:15:33 -0300

    meu terceiro

---
+++ Aulas/DesafioDoHeroiBatman.js
@@ -0,0 +1,27 @@
+let nomeHeroi = "Batman", nivelHeroi = "", nivelXp = 4510
+if (nivelXp <= 1000){ 
+     nivelHeroi = "Ferro"
+
+} else if (nivelXp <= 2000){
+    nivelHeroi = "Bronze"
+
+} else if (nivelXp <= 5000){
+    nivelHeroi = "Prata"
+
+} else if (nivelXp <= 7000){
+    nivelHeroi = "Ouro"
+
+} else if (nivelXp <= 8000){
+    nivelHeroi = "Platina"
+
+} else if (nivelXp <= 9000){
+    nivelHeroi = "Ascendente"
+
+} else if (nivelXp <= 10000){
+    nivelHeroi = "Imortal"
+
+} else {
+    nivelHeroi = "Radiante"
+    
+} 
+console.log ("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
